993cc252211d4ec71de354a264836df02ad88856
sbird <sbird@no.tld>
2026-05-31 12:08:07 +0200

    match SetRAMBuildDate

---
+++ include/file_table.h
@@ -0,0 +1,14 @@
+#ifndef _FILE_TABLE_H_
+#define _FILE_TABLE_H_
+
+// external symbols from the file_table.ld linker script
+extern int _FILE_BUILD_DATE;
+
+// friendly names for C
+#define FILE_BUILD_DATE ((int)&_FILE_BUILD_DATE)
+
+// global forward declarations
+
+void *GetFile(int index);
+
+#endif // _FILE_TABLE_H_
