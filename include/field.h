72fe8dbfe0bd35c2f41777bc18cf8194a6c0582c
sbird <sbird@no.tld>
2026-06-07 13:37:47 +0200

    match 2 map related functions

--- include/field.h
+++ include/field.h
@@ -10,7 +10,9 @@ struct MapState {
     const void * actors[65]; // Actor
     u8 __unk118[0x46];
     bool16 startPressed;
-    u8 __unk160[0x40];
+    u8 __unk160[0x1E];
+    u16 __unk17E;
+    u8 __unk180[0x20];
     u8 __unk1A0[0x8];
     const void *tilemap;
     u8 __unk1AC[0x14];
