67999dd53df1150ec65cfb0e4008df541a981cec
sbird <sbird@no.tld>
2026-06-21 16:21:16 +0200

    finish allocators

--- src/rom_c0/rom_4858_a_b.c
+++ src/rom_c0/rom_4858_a_b.c
@@ -35,11 +35,11 @@ void *galloc_iwram(s32 index, u32 size) {
 
     ptr = gPtrs[index];
 
-    if (ptr != 0) return ptr;
+    if (ptr != NULL) return ptr;
 
     ptr = gPtrs[1];
     size = ((size + 3) / 4) * 4;
-    if (ptr + size > (void*)0x030077ff) {
+    if (ptr + size >= (void*)0x03007800) {
         ewramPtr = gPtrs[0];
         if (ewramPtr + size >= (void*)0x02040000) {
             return NULL;
@@ -50,5 +50,63 @@ void *galloc_iwram(s32 index, u32 size) {
     }
     gPtrs[1] = ptr + size;
     gPtrs[index] = ptr;
+    return ptr;
+}
+
+void *galloc_ewram(s32 index, u32 size) {
+    void *iwramPtr;
+    void *ptr;
+
+    ptr = gPtrs[index];
+
+    if (ptr != NULL) return ptr;
+
+    ptr = gPtrs[0];
+    size = ((size + 3) / 4) * 4;
+    if (ptr + size >= (void*)0x02040000) {
+        iwramPtr = gPtrs[1];
+        if (iwramPtr + size >= (void*)0x03007800) {
+            return NULL;
+        }
+        gPtrs[1] = iwramPtr + size;
+        gPtrs[index] = iwramPtr;
+        return iwramPtr;
+    }
+    gPtrs[0] = ptr + size;
+    gPtrs[index] = ptr;
+    return ptr;
+}
+
+void *Func_8004938(u32 size) {
+    void *ptr;
+
+    ptr = gPtrs[1];
+    size = ((size + 3) >> 2) * 4;
+    if (ptr + size >= (void*)0x03007800) {
+        ptr = gPtrs[0];
+        if (ptr + size >= (void*)0x02040000) {
+            return NULL;
+        }
+        gPtrs[0] = ptr + size;
+        return ptr;
+    }
+    gPtrs[1] = ptr + size;
+    return ptr;
+}
+
+void *Func_8004970(u32 size) {
+    void *ptr;
+
+    ptr = gPtrs[0];
+    size = ((size + 3) >> 2) * 4;
+    if (ptr + size >= (void*)0x02040000) {
+        ptr = gPtrs[1];
+        if (ptr + size >= (void*)0x03007800) {
+            return NULL;
+        }
+        gPtrs[1] = ptr + size;
+        return ptr;
+    }
+    gPtrs[0] = ptr + size;
     return ptr;
 }
