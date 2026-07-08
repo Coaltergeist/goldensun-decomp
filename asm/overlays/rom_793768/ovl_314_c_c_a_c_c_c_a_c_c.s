6473d2d502b57555548b9071beb811623d6b17cb
Coaltergeist <coaltergeist@gmail.com>
2026-07-08 09:14:06 -0500

    4 matches

--- asm/overlays/rom_793768/ovl_314_c_c_a_c_c_c_a_c_c.s
+++ asm/overlays/rom_793768/ovl_314_c_c_a_c_c_c_a_c_c.s
@@ -1,39 +1,5 @@
 	.include "macros.inc"
 
-.thumb_func_start OvlFunc_898_2008b98
-	push	{lr}
-	bl	__CutsceneStart
-	ldr	r0, =0x133f
-	bl	__MessageID
-	mov	r0, #0x12
-	mov	r1, #0
-	mov	r2, #2
-	bl	OvlFunc_898_200973c
-	mov	r1, #0
-	mov	r0, #0x12
-	bl	__Func_8092c40
-	mov	r0, #0
-	mov	r1, #0
-	bl	__Func_8091c7c
-	cmp	r0, #0
-	beq	.Lbd2
-	ldr	r3, =iwram_3001ebc
-	ldr	r2, [r3]
-	mov	r3, #0xec
-	lsl	r3, #1
-	add	r2, r3
-	ldrh	r3, [r2]
-	add	r3, #1
-	strh	r3, [r2]
-.Lbd2:
-	mov	r0, #0x12
-	mov	r1, #0
-	bl	__ActorMessage
-	bl	__CutsceneEnd
-	pop	{r0}
-	bx	r0
-.func_end OvlFunc_898_2008b98
-
 .thumb_func_start OvlFunc_898_2008bec
 	push	{lr}
 	bl	__CutsceneStart
