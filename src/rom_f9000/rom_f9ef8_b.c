802755bc6e67ae398579a7371c0956d98d1afef5
Coaltergeist <coaltergeist@gmail.com>
2026-08-12 02:23:58 -0500

    Removed orphans; all c code is removed from rom_* folders

--- src/rom_f9000/rom_f9ef8_b.c
+++
@@ -1,8 +0,0 @@
-/* Cluster UnusedDummyFunc..UnusedDummyFunc extracted from goldensun/rom_f9000/src/rom_f9ef8.s.
- *
- * Total .text for this TU = 4 bytes (= 0x4).
- * Preserves the original ROM layout when slotted between
- * rom_f9000/src/rom_f9ef8_a.o and rom_f9000/src/rom_f9ef8_c.o in
- * goldensun/stage1.ld.
- */
-void UnusedDummyFunc(void) {}
