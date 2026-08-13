c9884c1bdebaa5212695dd1c81143dbf72fa6aa7
Coaltergeist <coaltergeist@gmail.com>
2026-08-13 17:57:00 -0500

    intr/main/video cluster split

--- src/video.c
+++ src/video.c
@@ -16,6 +16,101 @@ extern vu8 iwram_3001ca8;
 extern u8 iwram_3001cd4;
 extern u16 iwram_3001cf8;
 
+#include "gba/types.h"
+#include "gba/io.h"
+#include "dma.h"
+
+struct DmaTransfer {
+    const void *src;
+    void *dest;
+    u32 control;
+};
+
+struct DmaQueue {
+    u16 count;
+    struct DmaTransfer tasks[32];
+};
+
+extern struct DmaQueue gDMATaskCount;
+
+static inline void ScheduleDmaTransfer(void *dest, const void *src, u32 cnt) {
+    struct DmaQueue *queue;
+    register void *d __asm__("r6");
+    register const void *s __asm__("r0");
+    u32 savedIme;
+    s32 count;
+    u32 *task;
+
+    queue = &gDMATaskCount;
+    __asm__ volatile("" : : "r"(queue));
+    d = dest;
+    s = src;
+    savedIme = REG_IME;
+    SET_IO(REG_IME, REG_ADDR_IME);
+    count = queue->count;
+    if (count < 32) {
+        task = (u32 *)(count * 12 + (u32)queue + 4);
+        *task++ = (u32)s;
+        queue->count = count + 1;
+        *task++ = (u32)d;
+        *task = cnt;
+    }
+    SET_IO(REG_IME, savedIme);
+}
+
+void Func_800383c(void *dest, const void *src) {
+    ScheduleDmaTransfer(dest, src, 0x10000);
+}
+
+void SetRegAnimDest(void *dest, const void *src) {
+    ScheduleDmaTransfer(dest, src, 0x20000);
+}
+
+void Func_80038bc(void *dest, const void *src) {
+    ScheduleDmaTransfer(dest, src, 0x30000);
+}
+
+void Func_80038fc(void *dest, const void *src) {
+    ScheduleDmaTransfer(dest, src, 0x50000);
+}
+
+void Func_800393c(void *dest, const void *src) {
+    ScheduleDmaTransfer(dest, src, 0x60000);
+}
+
+void Func_800397c(void *dest, const void *src) {
+    ScheduleDmaTransfer(dest, src, 0x70000);
+}
+
+void Func_80039bc(void *dest, const void *src) {
+    ScheduleDmaTransfer(dest, src, 0x90000);
+}
+
+void Func_80039fc(void *dest, const void *src) {
+    ScheduleDmaTransfer(dest, src, 0xA0000);
+}
+
+void Func_8003a3c(void *dest, const void *src) {
+    ScheduleDmaTransfer(dest, src, 0xB0000);
+}
+
+extern void UploadPalette_ROM(struct DmaQueue *queue, u32 count);
+
+extern int _UPLOAD_PALETTE_SIZE;
+
+void UploadPalette(void) {
+    u32 count = gDMATaskCount.count;
+    if (count != 0) {
+        u32 size = (u32)&_UPLOAD_PALETTE_SIZE;
+        u32 funcBuffer[size / 4];
+        void (*func)(struct DmaQueue *, u32);
+        DMA3_COPY(UploadPalette_ROM, funcBuffer, size);
+        func = (void (*)(struct DmaQueue *, u32))funcBuffer;
+        func(&gDMATaskCount, count);
+        gDMATaskCount.count = 0;
+    }
+}
+
 void Func_8003adc(void) {
     s32 temp1;
     u32 temp2;
