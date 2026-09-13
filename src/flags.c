07788d122da36b60d74a947b9b3e47aba29b1f1e
Coaltergeist <coaltergeist@gmail.com>
2026-09-12 23:02:14 -0500

    2 dehacks

--- src/flags.c
+++ src/flags.c
@@ -10,28 +10,18 @@ extern unsigned char gFlags[];
 
 int ToggleFlag(int flagID)
 {
-    register int f __asm__("r4") = flagID;
-    register unsigned int bit __asm__("r1");
-    register unsigned int tmp __asm__("r3");
-    register unsigned int idx __asm__("r4");
-    register unsigned char *t __asm__("r2");
-    unsigned int bc;
-    int v;
+    unsigned int bit;
+    unsigned int index;
+    unsigned char *table;
+    int value;
 
-    __asm__ volatile ("" : : "r" (f));
-    bit = 1;
-    bit <<= f & 7;
-    __asm__ volatile ("" : : "r" (bit));
-    t = gFlags;
-    __asm__ volatile ("" : : "r" (t));
-    tmp = (unsigned int)f << 20;
-    __asm__ volatile ("" : : "r" (tmp));
-    idx = tmp >> 23;
-    __asm__ volatile ("" : : "r" (idx));
-    __asm__ ("" : "=r" (bc) : "0" (bit));
-    t[idx] = bc ^ t[idx];
-    v = t[idx] & bit;
-    return (unsigned int)(-v | v) >> 31;
+    bit = 1u << (flagID & 7);
+    table = gFlags;
+    flagID = ((unsigned int)flagID << 20) >> 23;
+    index = flagID;
+    table[index] ^= bit;
+    value = table[index] & bit;
+    return (unsigned int)(-value | value) >> 31;
 }
 
 unsigned char GetFlagByte(unsigned int id) {
@@ -61,23 +51,18 @@ int GetFlagNybble(unsigned int id) {
 
 void SetFlagNybble(int flagID, int value)
 {
-    register int f __asm__("r6") = flagID;
-    register unsigned char *t __asm__("r4") = gFlags;
-    register unsigned int tmp __asm__("r3");
-    register int shift __asm__("r0");
-    register int mask __asm__("r5");
-    register unsigned int idx __asm__("r6");
+    unsigned int shift;
+    unsigned int mask;
+    unsigned int indexBits;
+    unsigned int keepMask;
+    unsigned char *table;
 
-    __asm__ volatile ("" : : "r" (f));
-    __asm__ volatile ("" : : "r" (t));
-    tmp = (unsigned int)f << 20;
-    __asm__ volatile ("" : : "r" (tmp));
-    shift = 4;
-    shift &= f;
-    __asm__ volatile ("" : : "r" (shift));
-    mask = 0xf;
-    __asm__ volatile ("" : : "r" (mask));
-    idx = tmp >> 23;
-    __asm__ volatile ("" : : "r" (idx));
-    t[idx] = (t[idx] & ~(mask << shift)) | ((value & mask) << shift);
+    mask = 15;
+    shift = flagID;
+    shift = shift & 4;
+    indexBits = (unsigned int)flagID << 20;
+    table = gFlags;
+    keepMask = ~(mask << shift);
+    flagID = indexBits >> 23;
+    table[flagID] = (keepMask & table[flagID]) | ((value & mask) << shift);
 }
